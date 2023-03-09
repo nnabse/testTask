cbcbf31dc46aa842393f4ec3f55c10de8f07290f
nnabse <nnabse@gmail.com>
2023-03-09 20:40:47 +0300

    add auth guard, edit tsconfig

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthGuard } from '@guards/auth.guard';
 import { AuthComponent } from '@pages/auth/auth.component';
 import { MainComponent } from '@pages/main/main.component';
 
@@ -8,7 +9,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'main', pathMatch: 'full' },
   { path: 'signIn', component: AuthComponent },
   { path: 'signUp', component: AuthComponent },
-  { path: 'main', component: MainComponent },
+  { path: 'main', component: MainComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: 'main', pathMatch: 'full' },
 ];
 
