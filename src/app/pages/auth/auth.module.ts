9e75614db2aec59a9010d6a052cf96ba01f531a9
nnabse <nnabse@gmail.com>
2023-03-10 14:46:13 +0300

    change styles, replace my styles with material's buttons etc

--- src/app/pages/auth/auth.module.ts
+++ src/app/pages/auth/auth.module.ts
@@ -4,12 +4,19 @@ import { NgModule } from '@angular/core';
 
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { MaterialModule } from '@material/material.module';
 import { AuthComponent } from '@pages/auth/auth.component';
 import { FormComponent } from '@pages/auth/form/form.component';
 
 @NgModule({
   declarations: [FormComponent, AuthComponent],
-  imports: [CommonModule, RouterLink, ReactiveFormsModule, HttpClientModule],
+  imports: [
+    CommonModule,
+    RouterLink,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MaterialModule,
+  ],
   exports: [AuthComponent, FormComponent],
 })
 export class AuthModule {}
